541a59a4b352b8c932d2707bea97b5294ccd8b56
Philip Oyoo <philipoyoo11@gmail.com>
2025-12-09 11:14:57 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,14 @@
--- task_4.sql
--- Print the full description of the table 'books' from alx_book_store database
--- Without using DESCRIBE or EXPLAIN
-
-SHOW CREATE TABLE books;
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books'
+ORDER BY
+    ORDINAL_POSITION;
