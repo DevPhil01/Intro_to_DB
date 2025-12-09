cd8e39c0397db54c4a2f22baee5ece262739e353
Philip Oyoo <philipoyoo11@gmail.com>
2025-12-09 11:20:48 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,16 @@
 -- task_4.sql
--- Full description of the table books without using DESCRIBE or EXPLAIN
 
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    DATA_TYPE, 
-    CHARACTER_MAXIMUM_LENGTH, 
-    IS_NULLABLE, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'Books'
-ORDER BY 
+ORDER BY
     ORDINAL_POSITION;
